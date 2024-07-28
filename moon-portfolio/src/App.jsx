ae418ed78d5523e73294dfb7ac3bfcb6cc2c521d
smy1999 <865988027@qq.com>
2024-07-28 23:03:13 +0800

    CSS setup.

--- moon-portfolio/src/App.jsx
+++ moon-portfolio/src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
-import './App.module.css'
+import styles from './App.module.css'
 
 function App() {
 
-  return (
-    <>
-     Hello world
-    </>
-  )
+  return <div className={styles.App}>Hello world</div>
+
 }
 
 export default App
