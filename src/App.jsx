7870c36cf78e421c2017c11148284129d74347ec
smy1999 <865988027@qq.com>
2024-07-30 22:17:07 +0800

    [Feature] Experience.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styles from './App.module.css'
 import {Navbar} from "./components/Navbar/Navbar.jsx";
 import {Hero} from "./components/Hero/Hero.jsx";
 import {About} from "./components/About/About.jsx";
+import {Experience} from "./components/Experience/Experience.jsx";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <About/>
+      <Experience/>
     </div>
   )
 }
