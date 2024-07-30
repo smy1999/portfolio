3b0a11e9d8bd57b03721ac0c1887b04368104b1b
smy1999 <865988027@qq.com>
2024-07-30 15:13:02 +0800

    [Feature] About.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import styles from './App.module.css'
 import {Navbar} from "./components/Navbar/Navbar.jsx";
 import {Hero} from "./components/Hero/Hero.jsx";
+import {About} from "./components/About/About.jsx";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div className={styles.App}>
       <Navbar/>
       <Hero/>
+      <About/>
     </div>
   )
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable react/no-unescaped-entities */
+import styles from './About.module.css'
+import {getImageUrl} from "../../utils.js";
+
+export const About = () => {
+
+  return (
+    <section className={styles.container} id={"about"}>
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img className={styles.aboutImage}
+             alt={"Me sitting with a laptop"}
+             src={getImageUrl("about/aboutImage.png")}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img className={styles.aboutItemImg}
+                 alt={"Cursor icon"}
+                 src={getImageUrl("about/cursorIcon.png")}
+            />
+            <div className={styles.aboutItemText}>
+              <h3 >Frontend Developer</h3>
+              <p >I'm a front-end developer with experience in building responsive
+                and optimized sites</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img className={styles.aboutItemImg}
+                 alt={"Server icon"}
+                 src={getImageUrl("about/serverIcon.png")}
+            />
+            <div className={styles.aboutItemText}>
+              <h3 >Backend Developer</h3>
+              <p >I have experience developing fast and optimized back-end systems
+                and APIs</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img className={styles.aboutItemImg}
+                 alt={"Ui icon"}
+                 src={getImageUrl("about/uiIcon.png")}
+            />
+            <div className={styles.aboutItemText}>
+              <h3>Machine Learning Developer</h3>
+              <p>I'm a machine learning developer with experience in training and
+                implementing efficient models.</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
