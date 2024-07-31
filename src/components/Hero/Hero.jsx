bf51351d0e132928a8a181998161a87bf71d486d
smy1999 <865988027@qq.com>
2024-08-01 00:12:42 +0800

    [Fix] smooth scrolling.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import styles from "./Hero.module.css"
 
 export const Hero = () => {
   return (
-    <section className={styles.container}>
+    <section className={styles.container} id={"hero"}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Mingyue</h1>
         <p className={styles.description}>I'm a full-stack developer with 2 years of experience using Java and React. Reach out if you'd like to learn more.</p>
