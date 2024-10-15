565bd5c9e2bcf306595f7778f8468dd6e4b973d2
smy1999 <smy19990312@gmail.com>
2024-10-14 21:07:15 -0700

    [Feature] AWS [Comment] Test

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -1,17 +1,28 @@
 import { getUrl } from 'aws-amplify/storage';
+import outputs from "../../../amplify_outputs.json";
+import {Amplify} from "aws-amplify";
+import { generateClient } from "aws-amplify/data";
 
-const linkToStorageFile = await getUrl({
-  path: "album/2024/1.jpg",
-  // Alternatively, path: ({identityId}) => `album/{identityId}/1.jpg`
+// const linkToStorageFile = await getUrl({
+//   path: "data/1.jpg",
+//   // Alternatively, path: ({identityId}) => `album/{identityId}/1.jpg`
+// });
+
+/**
+ * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
+ */
+
+Amplify.configure(outputs);
+const client = generateClient({
+  authMode: "userPool",
 });
-
 
 export const Test = () => {
   return (
     <div>
-      <a href={linkToStorageFile.url.toString()} target="_blank" rel="noreferrer">
-        {"fileName"}
-      </a>
+      {/*<a href={linkToStorageFile.url.toString()} target="_blank" rel="noreferrer">*/}
+      {/*  {"fileName"}*/}
+      {/*</a>*/}
     </div>
   )
 }
