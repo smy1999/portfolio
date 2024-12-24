56b5f30f9d4212898a6b7299f3b2ac631f0c316a
smy1999 <smy19990312@gmail.com>
2024-12-23 21:08:46 -0800

    [Feature] AWS [Comment] Get experiences and projects from aws

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,37 @@
 import styles from "./Projects.module.css";
-import projects from "../../data/projects.json"
 import {ProjectCard} from "./ProjectCard.jsx";
+import {useEffect, useState} from "react";
+import {get} from "../../utils.js";
 export const Projects = () => {
+
+  const [projects, setProjects] = useState()
+
+  const getProjects = async () => {
+    try {
+      const response = await get('/projects');
+      console.log(2)
+      console.log(response);
+
+      if (response.body) {
+        const data = JSON.parse(response.body);
+        const mappedProjects = data.projects.map(project => ({
+          ...projects,
+          imageSrc: project.image_src
+        }));
+        console.log(mappedProjects);
+        setProjects(mappedProjects);
+      } else {
+        console.error('Response body is undefined');
+      }
+    } catch (error) {
+      console.error('Error fetching skills:', error);
+    }
+  };
+  useEffect(() => {
+    getProjects();
+  }, []);
+
+
   return (
     <section className={styles.container} id={"projects"}>
       <h2 className={styles.title}>Projects</h2>
